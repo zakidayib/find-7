b21d00bf51728ab7a53ed5a0121b1d82674ac8fe
zaki tayyip <zakidayib1997@gmail.com>
2023-06-05 22:47:43 +0300

    find seven solved

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+
+let findSeven = number => {
+    for(let i=0; i<number.length; i++){
+        if(number[i] === 7){
+            return "found";
+        }
+
+        else {
+            return "found";
+        }
+    }
+}
+
+console.log(findSeven([1,2,3,4,5,6]))
